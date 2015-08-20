9883594497c61dad5a23a66f6b658f61cd83c82b
PeeHaa <info@pieterhordijk.com>
2015-08-20 23:43:33 +0200

    Added exceptions

---
+++ src/InvalidException.php
@@ -0,0 +1,25 @@
+<?php declare(strict_types=1);
+/**
+ * Exception which gets thrown when trying to load a theme which is not valid
+ *
+ * PHP version 7.0
+ *
+ * @category   CodeCollab
+ * @package    Theme
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2015 Pieter Hordijk <https://github.com/PeeHaa>
+ * @license    See the LICENSE file
+ * @version    1.0.0
+ */
+namespace CodeCollab\Theme;
+
+/**
+ * Exception which gets thrown when trying to load a theme which is not valid
+ *
+ * @category   CodeCollab
+ * @package    Theme
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ */
+class InvalidException extends \Exception
+{
+}
